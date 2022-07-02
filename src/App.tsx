c0f73d1c30e0d67ae6b2d481ebe1d822f04e0e10
luiizsilverio <luiiz.silverio@gmail.com>
2022-07-02 17:16:20 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from './store/Stock.store';
+import { RootState } from './store';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch()
+  const stock = useSelector((state: RootState) => state.stock)
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>{ stock.counter }</h1>
+        <button onClick={() => dispatch(increment())}>Somar</button>
+        <button onClick={() => dispatch(decrement())}>Subtrair</button>
       </header>
     </div>
   );
